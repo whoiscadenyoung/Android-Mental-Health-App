c2a170e14464078b97c193732e1daa5d69ccfb53
Vasti Sarai Rios Rios <vriosrio@trinity.edu>
2021-04-25 15:18:48 -0500

    Bottom Navigation and Map Fragment

---
+++ app/src/main/java/com/csci3397/cadenyoung/groupproject/ui/map/MapViewModel.java
@@ -0,0 +1,18 @@
+package com.csci3397.cadenyoung.groupproject.ui.map;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MapViewModel() {
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is dashboard fragment");
+    }
+
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
+}
