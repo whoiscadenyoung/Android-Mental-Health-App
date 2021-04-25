928d5936fb405f8c79d58be3b828e68598924ac7
ale <jtrujil1@gmail.com>
2021-04-25 14:46:33 -0500

    Login and register fragments.

---
+++ app/src/main/java/com/csci3397/cadenyoung/groupproject/ui/LoginFragment.java
@@ -0,0 +1,227 @@
+package com.csci3397.cadenyoung.groupproject.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.csci3397.cadenyoung.groupproject.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import static android.content.ContentValues.TAG;
+
+public class LoginFragment extends Fragment {
+
+    SignInButton signInButton;
+    Button loginButton;
+    Button registerPageButton;
+    EditText emailText;
+    EditText passwordText;
+    GoogleSignInClient googleSignInClient;
+    FirebaseAuth firebaseAuth;
+    String email;
+    String password;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+        signInButton = view.findViewById(R.id.signInButton);
+        loginButton = view.findViewById(R.id.loginButton);
+        registerPageButton = view.findViewById(R.id.registerPageButton);
+        emailText = view.findViewById(R.id.emailText);
+        passwordText = view.findViewById(R.id.passwordText);
+
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        //loginButton.setSize(SignInButton.SIZE_STANDARD);
+        //registerPageButton.setSize(SignInButton.SIZE_STANDARD);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailText.getText().toString();
+                password = passwordText.getText().toString();
+                signIn(email, password);
+            }
+
+        });
+
+        registerPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToRegisterPage();
+            }
+        });
+
+        //Initialize sign in options
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN
+        ).requestIdToken("766404629826-f0k3quh22862hjh376rdmkdia9m9obpe.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        //Initialize sign in client
+        googleSignInClient = GoogleSignIn.getClient(getActivity(), googleSignInOptions);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Initialize sign in intent
+                Intent intent = googleSignInClient.getSignInIntent();
+                //Start activity for result
+                startActivityForResult(intent, 100);
+            }
+        });
+
+        //Initialize firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        //Initialize firebase user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        //Check condition
+        if (firebaseUser != null) {
+            //When user is already signed in
+            //Redirect to homepage
+            moveToHomepage();
+        }
+        return view;
+    }
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        //Check if user is signed in (non-null) and update UI accordingly
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser != null) moveToHomepage();
+    }*/
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Check condition
+        if(requestCode == 100) {
+            //When request code is equal to 100
+            //Initialize task
+            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+            //Check condition
+            if(signInAccountTask.isSuccessful()) {
+                //When google sign in successful
+                //Initialize string
+                String s = "Google sign in successful";
+                //Display toast
+                Toast.makeText(getActivity().getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    //Initialize sign in account
+                    GoogleSignInAccount googleSignInAccount = signInAccountTask
+                            .getResult(ApiException.class);
+                    //Check condition
+                    if(googleSignInAccount != null) {
+                        //When sign in account is not equal to null
+                        //Initialize auth credential
+                        AuthCredential authCredential = GoogleAuthProvider
+                                .getCredential(googleSignInAccount.getIdToken(),
+                                        null);
+                        //Check credential
+                        firebaseAuth.signInWithCredential(authCredential)
+                                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        //Check condition
+                                        if(task.isSuccessful()) {
+                                            //When task is successful
+                                            //Redirect to homepage
+                                            getUser();
+                                            moveToHomepage();
+                                            Toast.makeText(getActivity(), "sign in successful" , Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(getActivity(), "sign in UNsuccessful" , Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void getUser() {
+    }
+
+
+    private void signIn(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            getUser();
+                            moveToHomepage();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed. Incorrect email or password.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                            //checkForMultiFactorFailure(task.getException());
+                        }
+
+                        /*if (!task.isSuccessful()) {
+                            mBinding.status.setText(R.string.auth_failed);
+                        }*/
+                    }
+                });
+    }
+
+
+    private void moveToHomepage() {
+        /*fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, new HomeFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();*/
+    }
+
+    private void moveToRegisterPage() {
+        fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, new RegisterFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/csci3397/cadenyoung/groupproject/ui/RegisterFragment.java
@@ -0,0 +1,96 @@
+package com.csci3397.cadenyoung.groupproject.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.csci3397.cadenyoung.groupproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+public class RegisterFragment extends Fragment {
+
+    Button registerButton;
+    EditText newEmailText;
+    EditText newPasswordText;
+    EditText firstNameText;
+    EditText lastNameText;
+    FirebaseAuth firebaseAuth;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        registerButton = view.findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = newEmailText.getText().toString();
+                String password = newPasswordText.getText().toString();
+                createAccount(email, password);
+            }
+        });
+        return view;
+    }
+
+    private void createNewUser() {
+        String firstName = firstNameText.getText().toString();
+        String lastName = lastNameText.getText().toString();
+    }
+
+    private void createAccount(String email, String password){
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            //updateUI(user);
+                            createNewUser();
+                            moveToHomepage();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void moveToHomepage() {
+        /*fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, new HomeFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();*/
+    }
+}
