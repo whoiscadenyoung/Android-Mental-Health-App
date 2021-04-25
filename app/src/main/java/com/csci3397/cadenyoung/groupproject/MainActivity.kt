b712d0b0eb23c0947ad99440843f9c52e940a3a6
ale <jtrujil1@gmail.com>
2021-04-25 14:44:35 -0500

    Login and register fragments.

--- app/src/main/java/com/csci3397/cadenyoung/groupproject/MainActivity.kt
+++ app/src/main/java/com/csci3397/cadenyoung/groupproject/MainActivity.kt
@@ -2,10 +2,31 @@ package com.csci3397.cadenyoung.groupproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.csci3397.cadenyoung.groupproject.ui.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*var navController = Navigation.findNavController(this, R.id.fragment)
+        //NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        var appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.loginJavaFragment,
+            R.id.registerJavaFragment
+        ).build()
+        NavigationUI.setupActionBarWithNavController(
+            this, navController,
+            appBarConfiguration
+        )*/
+
+        var fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,
+            LoginFragment()
+        )
+        fragmentTransaction.commit()
+
     }
 }
