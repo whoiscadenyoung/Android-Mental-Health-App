b0b3327bfe82b7a11311fe6302e69c5cf35ed1e4
whoiscadenyoung <56892869+whoiscadenyoung@users.noreply.github.com>
2021-05-05 16:23:58 -0500

    Fixed Stats to take and receive UserStats

--- app/src/main/java/com/csci3397/cadenyoung/groupproject/model/Stat.java
+++ app/src/main/java/com/csci3397/cadenyoung/groupproject/model/Stat.java
@@ -2,26 +2,29 @@ package com.csci3397.cadenyoung.groupproject.model;
 
 public class Stat {
     private int imageId;
+    private int id;
     private String name;
     private int descId;
     private int colorId;
 
     private int progress;
 
-    public Stat(int imageId, String name, int descId, int colorId) {
+    public Stat(int imageId, int id, String name, int descId, int colorId) {
         this.imageId = imageId;
         this.name = name;
         this.descId = descId;
         this.progress = 0;
         this.colorId = colorId;
+        this.id = id;
     }
 
-    public Stat(int imageId, String name, int descId, int colorId, int progress) {
+    public Stat(int imageId, int id, String name, int descId, int colorId, int progress) {
         this.imageId = imageId;
         this.name = name;
         this.descId = descId;
         this.progress = progress;
         this.colorId = colorId;
+        this.id = id;
     }
 
     public int getProgress() {return this.progress;}
